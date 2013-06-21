624ba9905a0f25476135d0b2984dc40dc431a422
rob <rob@localhost.(none)>
2013-06-21 10:29:18 -0400

    gitignore aptana .project

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,23 @@ ActiveRecord::Schema.define(:version => 20130610124218) do
 
   add_index "pages", ["slug"], :name => "index_pages_on_slug"
 
+  create_table "redactor_assets", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "data_file_name",                  :null => false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    :limit => 30
+    t.string   "type",              :limit => 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+  end
+
+  add_index "redactor_assets", ["assetable_type", "assetable_id"], :name => "idx_redactor_assetable"
+  add_index "redactor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_redactor_assetable_type"
+
   create_table "roles", :force => true do |t|
     t.string   "name"
     t.string   "key"
