02aa7e6cd0409cf3aa5e95005838a55298971e72
Tim Boisvert <tboisvert@asheavenue.com>
2013-07-07 12:08:28 -0400

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,29 +10,77 @@ user.save
 user.add_role :superadmin
 
 # Create the components
-c = Component.new
-c.name = 'Meta'
-c.slug = 'meta'
+m = Component.new
+m.name = 'Meta'
+m.slug = 'meta'
+m.save!
+b = Component.new
+b.name = 'Body'
+b.slug = 'body'
+b.save!
+s = Component.new
+s.name = 'Sound'
+s.slug = 'sound'
+s.save!
+v = Component.new
+v.name = 'Video'
+v.slug = 'video'
+v.save!
+i = Component.new
+i.name = 'Image'
+i.slug = 'image'
+i.save!
+g = Component.new
+g.name = 'Gallery'
+g.slug = 'gallery'
+g.save!
+
+# Create templates with the components
+t = Template.new
+t.name = "Video"
+t.slug = "video"
+t.image_url = "http://nicenicejpg.com/180/250"
+t.save!
+t.template_components.create(:component => v)
+t.template_components.create(:component => m)
+t.template_components.create(:component => b)
+t.save!
+t = Template.new
+t.name = "Sound"
+t.slug = "sound"
+t.image_url = "http://nicenicejpg.com/180/250"
+t.save!
+t.template_components.create(:component => i)
+t.template_components.create(:component => m)
+t.template_components.create(:component => s)
+t.template_components.create(:component => b)
+t.save!
+t = Template.new
+t.name = "Gallery"
+t.slug = "gallery"
+t.image_url = "http://nicenicejpg.com/180/250"
+t.save!
+t.template_components.create(:component => g)
+t.template_components.create(:component => m)
+t.template_components.create(:component => b)
+t.save!
+
+# Create test categories
+c = Category.new
+c.name = "Test Category 1"
+c.slug = "test-category-1"
 c.save!
-c = Component.new
-c.name = 'Body'
-c.slug = 'body'
-c.save!
-c = Component.new
-c.name = 'Sound'
-c.slug = 'sound'
-c.save!
-c = Component.new
-c.name = 'Video'
-c.slug = 'video'
-c.save!
-c = Component.new
-c.name = 'Image'
-c.slug = 'image'
-c.save!
-c = Component.new
-c.name = 'Gallery'
-c.slug = 'gallery'
+c = Category.new
+c.name = "Test Category 2"
+c.slug = "test-category-2"
 c.save!
 
-
+# Create test series
+s = Series.new
+s.name = "Test Series 1"
+s.slug = "test-series-1"
+s.save!
+s = Series.new
+s.name = "Test Series 2"
+s.slug = "test-series-2"
+s.save!
