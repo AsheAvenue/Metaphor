cb623e7275aa13e87bbefce01436210d5e2c416b
Tim Boisvert <tboisvert@asheavenue.com>
2013-07-07 11:39:28 -0400

    fixed collection pinned article error

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+# Superadmin user
 user = User.new
 user.username = 'admin'
 user.password = 'admin'
@@ -13,4 +6,33 @@ user.email    = 'admin@sitename.com'
 user.display_name = 'admin'
 user.save
 
+# Give user Superadmin role
 user.add_role :superadmin
+
+# Create the components
+c = Component.new
+c.name = 'Meta'
+c.slug = 'meta'
+c.save!
+c = Component.new
+c.name = 'Body'
+c.slug = 'body'
+c.save!
+c = Component.new
+c.name = 'Sound'
+c.slug = 'sound'
+c.save!
+c = Component.new
+c.name = 'Video'
+c.slug = 'video'
+c.save!
+c = Component.new
+c.name = 'Image'
+c.slug = 'image'
+c.save!
+c = Component.new
+c.name = 'Gallery'
+c.slug = 'gallery'
+c.save!
+
+
