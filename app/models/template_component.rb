3d89377d1f7111a1e2a75103540b95468abed5b7
Tim Boisvert <tboisvert@asheavenue.com>
2013-07-23 12:46:27 -0400

    added order to attr_accessible for template_component

--- app/models/template_component.rb
+++ app/models/template_component.rb
@@ -1,5 +1,5 @@
 class TemplateComponent < ActiveRecord::Base
-  attr_accessible :template, :component
+  attr_accessible :template, :component, :order
   belongs_to :template 
   belongs_to :component
 end
