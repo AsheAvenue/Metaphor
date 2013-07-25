d4a9da035d029503708eeac97e2dec763b2c2fe9
Tim Boisvert <tboisvert@asheavenue.com>
2013-07-25 16:01:40 -0400

    updating style for name and slug in forms

--- app/models/related_party.rb
+++
@@ -1,5 +0,0 @@
-class RelatedParty < ActiveRecord::Base
-  attr_accessible :name, :slug
-  has_many :article_related_parties, :dependent => :destroy  
-  has_many :articles, :through => :article_related_parties
-end
