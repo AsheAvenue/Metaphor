886c4e3e084fda5d222ecdebe517585503a37144
Tim Boisvert <tboisvert@asheavenue.com>
2013-08-02 17:14:13 -0400

    article image size list on content page

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,7 +1,7 @@
 class Gallery < ActiveRecord::Base
   attr_accessible :name, :slug
   
-  has_many :entity_contents,
-           :as => :entity
+  has_many :entity_contents, :as => :entity
+  has_many :images, :through => :entity_contents, :source => :content, :source_type => "Image"
            
 end
