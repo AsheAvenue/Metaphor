70e9211bab898088c599b0969576147caa30b074
rob <rfarrell@asheavenue.com>
2013-08-01 21:36:00 -0400

    tidied up the Collection.get method

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,40 +1,17 @@
 class Collection < ActiveRecord::Base
   attr_accessible :article_type, :name, :order, :slug, :pinned_articles_attributes
   has_many :pinned_articles, :order => 'pinned_articles.order ASC'
+  has_many :articles, :through => :pinned_articles
   accepts_nested_attributes_for :pinned_articles
   
   def self.get(slug)
-    
     # get the collection defined by the slug
     c = Collection.find_by_slug(slug)
-    
-    # get the articles according to the scopes
-    generated = Article.article_type(c.article_type).sort(c.order).published.all
-    
-    # hold the pinned articles
-    pinned = []
-    
-    # loop through pinned articles and store them
-    c.pinned_articles.each do |pa|
-      pinned << pa.article
+    # combine pinned articles with all articles... uniquify
+    Article.article_type(c.article_type).sort(c.order).published.all.each do |a|
+      c.articles << a.current
     end
-
-    articles = pinned + generated
-    
-    # loop through all articles and remove duplicates
-    # also get the current version
-    ids_found = []
-    articles_to_be_returned = []
-    articles.each do |article|
-      if !ids_found.include? article.id
-        ids_found << article.id
-        articles_to_be_returned << article.current
-      end
-    end
-    
-    #return the articles
-    articles_to_be_returned
-    
+    c.articles.uniq
   end 
   
 end
