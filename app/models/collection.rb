d9073e3da0ab40106541380b2118108b8ea6e979
rob <rfarrell@asheavenue.com>
2013-08-02 14:01:26 -0400

    Collection.get uses dumb arrays due to array.push on active record resutls

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,7 +1,7 @@
 class Collection < ActiveRecord::Base
   attr_accessible :name, :slug, :article_type, :category, :series, :flag, :tag, :order, :pinned_articles_attributes
   has_many :pinned_articles, :order => 'pinned_articles.order ASC'
-  has_many :articles, :through => :pinned_articles
+  has_many :articles, :through => :pinned_articles, :conditions => "articles.last_published_revision_id IS NOT NULL"
   accepts_nested_attributes_for :pinned_articles
   
   def self.get(slug)
@@ -9,10 +9,16 @@ class Collection < ActiveRecord::Base
     c = Collection.find_by_slug(slug)
     if c
       # combine pinned articles with all articles... uniquify
+      generated = []
       Article.with_article_type(c.article_type).with_category(c.category).sort_by(c.order).published.all.each do |a|
-        c.articles << a.current
+        generated << a.current
       end
-      c.articles.uniq
+      
+      pinned = []
+      c.articles.each do |a|
+        pinned << a.current
+      end
+      (pinned + generated).uniq
     else
       # return nothing if the collection doesn't exist
       []
