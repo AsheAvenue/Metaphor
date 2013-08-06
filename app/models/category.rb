e8c534c7e238796b05dc05a41be057ee59b1786d
Tim Boisvert <tboisvert@asheavenue.com>
2013-08-06 12:37:09 -0400

    category getters

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,17 @@ class Category < ActiveRecord::Base
   has_many :articles, :through => :article_categories
   
   validates_presence_of :slug, :name
+  
+  
+  # GETTING FROM THE FRONTEND
+  # Pass in a category slug, get a list of articles
+  def self.get(slug)
+    c = Category.find_by_slug(slug)
+    if c
+      Article.with_category(c.id).published.sort_by('newest').all
+    else
+      []
+    end
+  end
+  
 end
