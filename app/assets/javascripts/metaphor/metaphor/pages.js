346bd733b0d42e9f9c019adfcf915b8a01bb45ca
Tim Boisvert <tboisvert@asheavenue.com>
2013-08-14 22:09:59 -0400

    Redactor for page content

--- app/assets/javascripts/metaphor/metaphor/pages.js
+++ app/assets/javascripts/metaphor/metaphor/pages.js
@@ -13,6 +13,17 @@ var pages = (function($) {
               $('#page-form').submit();
             });
             
+            //fire up redactor
+            $('#page-content').redactor({ 
+                convertImageLinks: true,
+                convertVideoLinks: true,
+                removeEmptyTags: false,
+                linkAnchor: false,
+                placeholder: 'Enter page text...',
+                buttons: ['html', '|', 'formatting', '|', 'bold', 'italic', 'deleted', '|', 'unorderedlist', 'orderedlist', '|', 'link', '|', 'horizontalrule'],
+                formattingTags: ['blockquote', 'h3']
+            });
+            
         }
         
     };
