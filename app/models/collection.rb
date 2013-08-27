7a7ece2974baf3ad6fc12a059374b41a456f2cf9
Tim Boisvert <tboisvert@asheavenue.com>
2013-08-27 10:35:30 -0400

    pinned-only collections

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,5 +1,5 @@
 class Collection < ActiveRecord::Base
-  attr_accessible :name, :slug, :content_type, :article_type, :category, :series, :flag, :tag, :limit, :order, :pinned_entities_attributes
+  attr_accessible :name, :slug, :content_type, :pinned_only, :article_type, :category, :series, :flag, :tag, :limit, :order, :pinned_entities_attributes
   
   has_many :pinned_entities, :order => 'pinned_entities.order ASC'
   has_many :entities, :through => :pinned_entities
@@ -12,20 +12,21 @@ class Collection < ActiveRecord::Base
     else
       c = Collection.find_by_slug(slug)
       if c
-        # combine pinned articles with all articles... uniquify
         generated = []
     
-        c.content_type.capitalize.constantize
-          .with_type(c.article_type)
-          .with_category(c.category)
-          .with_series(c.series)
-          .flagged_as(c.flag)
-          .sort_by(c.order)
-          .with_limit(c.limit)
-          .published
-          .each do |a|
-            generated << a.current
+        unless c.pinned_only
+          c.content_type.capitalize.constantize
+            .with_type(c.article_type)
+            .with_category(c.category)
+            .with_series(c.series)
+            .flagged_as(c.flag)
+            .sort_by(c.order)
+            .with_limit(c.limit)
+            .published
+            .each do |a|
+              generated << a.current
           end
+        end
         
         pinned = []
         c.pinned_entities.each do |e|
