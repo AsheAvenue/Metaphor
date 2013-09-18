2f99a9d186f6e777251cdf3db30ae99c7c727637
Tim Boisvert <tboisvert@asheavenue.com>
2013-09-18 17:17:27 -0400

    change cancan for vote

--- app/controllers/metaphor/articles_controller.rb
+++ app/controllers/metaphor/articles_controller.rb
@@ -6,6 +6,7 @@ module Metaphor
     layout 'metaphor/metaphor'
     
     before_filter :require_login, :except => :vote
+    load_and_authorize_resource :except => [:vote]
     
     def index
       @articles = Article.recently_created.limit(100)
