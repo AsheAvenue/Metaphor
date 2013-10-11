9ba7244916b3baf75247dac01d65ee7f5cb0ff18
Tim Boisvert <tboisvert@asheavenue.com>
2013-10-11 11:22:04 -0400

    change instagram to use user account only

--- app/controllers/metaphor/social_controller.rb
+++ app/controllers/metaphor/social_controller.rb
@@ -54,7 +54,12 @@ module Metaphor
       #where everything is generalized.
 
       @response = Rails.cache.fetch("social_instagram", :expires_in => 15.minutes) {
-        photos = Instagram.tag_recent_media(Settings.socials.instagram.hashtag)
+        Instagram.configure do |config|
+          config.client_id = Settings.socials.instagram.client_id
+          config.access_token = Settings.socials.instagram.access_token
+        end
+        
+        photos = Instagram.user_recent_media(Settings.socials.instagram.user_id, :count => 6 )
         items = []
         photos.each do |p|
           items << { "images" => p.images, "url" => p.link  }
