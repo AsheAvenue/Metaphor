2afca634d420a6cf3f3bce679865f1c58f148815
Tim Boisvert <tboisvert@asheavenue.com>
2015-02-10 20:23:24 -0500

    Stop form submission after first click.

--- app/assets/javascripts/metaphor/metaphor/mobile.js
+++ app/assets/javascripts/metaphor/metaphor/mobile.js
@@ -1,7 +1,8 @@
 // Logic for the article lightboxes
 var mobile = new function() {
 
-    var self = this;
+    var self = this,
+        submitted = false;
 
     this.init = function() {
         self.submitForm();
@@ -9,9 +10,12 @@ var mobile = new function() {
     
     this.submitForm = function() {
         $('.mobile .submit').on('click', function() {
-            $('#article_slug').val(slugify($('#article_title').val()));
-            $(this).text('Publishing...');
-            $(this).parents('form').submit();
+            if(submitted == false) {
+                $('#article_slug').val(slugify($('#article_title').val()));
+                $(this).text('Publishing...');
+                $(this).parents('form').submit();
+                submitted = true;
+            }
         });
     };
     
